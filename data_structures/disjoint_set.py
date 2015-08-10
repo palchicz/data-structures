8331328450a1d35cb70f11ce237adf53fa9f580f
Zachary Palchick <zpalchick@5amsolutions.com>
2015-08-09 17:25:41 -0700

    Adds find method

--- data_structures/disjoint_set.py
+++ data_structures/disjoint_set.py
@@ -1,14 +1,22 @@
 class DisjointSet():
 
+
     def __init__(self):
-        self._size = 0
+        self._parents = {}
+
 
     def add(self, element):
-        self._size += 1
+        self._parents[element] = element
+
 
     def add_many(self, elements):
         for element in elements:
             self.add(element)
 
+
+    def find(self, element):
+        return self._parents[element]
+
+
     def __len__(self):
-        return self._size
+        return len(self._parents)
