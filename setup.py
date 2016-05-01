ee0a0b87a42d695ec8b831ac51a84db18b2ee6f2
Zach Palchick <palchicz@users.noreply.github.com>
2016-04-30 17:24:59 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='data structures',
+      version='0.0.1',
+      description='Collection of helpful python data structures',
+      author='palchicz',
+      packages=['data_structures'])
+
+
